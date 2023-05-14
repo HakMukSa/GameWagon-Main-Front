6f9bd1302f8535ad89d06ed41e62047b6a0d421b
chapsaldduk <77143594+chapsaldduk@users.noreply.github.com>
2023-05-14 13:49:31 +0900

    GAMEWAGON-1 화면 깜빡임 이슈 수정 (#2)
    1. 화면 깜빡임 이슈 수정

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,21 +1,22 @@
+import Link from "next/link";
 export default function Nav() {
   return (
-    <div className="w-full h-[50px] bg-gray color-white flex">
-      <a href="/" className="mr-[50px]">
-        <span className="text-[30px]">Gamewagon</span>
-      </a>
-      <a href="/games" className="mr-[50px]">
-        <span className="text-[20px]">Games</span>
-      </a>
-      <a href="/community" className="mr-[50px]">
-        <span className="text-[20px]">Community</span>
-      </a>
-      <a href="/database">
-        <span className="text-[20px]">Database</span>
-      </a>
-      <a href="/login" className="absolute top-0 right-[30px]">
-        <span className="text-[20px]">Login</span>
-      </a>
+    <div className="w-[65%] h-[50px] bg-[#111827] color-white flex mx-auto">
+      <Link href="/" className="mr-[50px] ml-[10%]">
+        <span className="text-[30px]">게임왜건</span>
+      </Link>
+      <Link href="/games" className="mr-[50px] my-auto">
+        <span className="text-[20px]">게임</span>
+      </Link>
+      <Link href="/community" className="mr-[50px] my-auto">
+        <span className="text-[20px]">커뮤니티</span>
+      </Link>
+      <Link href="/database" className="my-auto">
+        <span className="text-[20px]">데이터베이스</span>
+      </Link>
+      <Link href="/login" className="absolute right-[30%] top-[1%]">
+        <span className="text-[20px]">로그인</span>
+      </Link>
     </div>
   );
 }
