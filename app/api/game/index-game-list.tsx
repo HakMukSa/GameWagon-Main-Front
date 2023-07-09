e47ead49ebbf4feccefe42ac2132f929e785c188
chapsaldduck <koala031532@gmail.com>
2023-07-09 16:02:45 +0900

    refactor: [games-game-list] 불필요 요소 삭제
    1. 불필요한 console.log() 제거

--- app/api/game/index-game-list.tsx
+++ app/api/game/index-game-list.tsx
@@ -13,7 +13,6 @@ export async function gameList10(
         params: { sortBy: sortBy },
       }
     );
-    console.log(data);
     return data;
   } catch (error: any) {
     if (!axios.isAxiosError(error)) {
